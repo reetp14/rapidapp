aa85d1a766e07ac49d8a6f6183c2892e59c29f82
Reet Pratayay <reet.p14@gmail.com>
2020-06-25 16:14:58 +0530

    added try/catch error handling

--- src/App.js
+++ src/App.js
@@ -2,34 +2,44 @@ import React from "react";
 import Mainlist from "./components/mainList";
 import "./App.css";
 import axios from "axios";
+require("dotenv").config();
 
 function App() {
   const [mainListItems, setMainListItems] = React.useState({ items: [] });
 
   React.useEffect(() => {
     const fetchData = async () => {
-      let results = await axios.get("https://www.reddit.com/r/aww.json", {});
+      try {
+        let results = await axios.get("https://www.reddit.com/r/aww.json", {});
+        console.log(results.data);
 
-      console.log(results.data);
-
-      setMainListItems({ items: results.data.data.children });
+        setMainListItems({ items: results.data.data.children });
+      } catch (error) {
+        alert(error);
+      }
     };
 
-    // const fetchPrice = async () => {
-    //   let price = await fetch("https://coingecko.p.rapidapi.com/simple/price", {
-    //     method: "GET",
-    //     mode: "no-cors"
-    //     headers: {
-    //       "x-rapidapi-host": "coingecko.p.rapidapi.com",
-    //       "x-rapidapi-key":
-    //         "3324f6e115msh4cc8dab6fc3f512p1ce99bjsn4395c68d95fc",
-    //     },
-    //   });
+    const fetchPrice = async () => {
+      let price = await axios({
+        method: "GET",
+        url: "https://coingecko.p.rapidapi.com/simple/price",
+        headers: {
+          "content-type": "application/octet-stream",
+          "x-rapidapi-host": "coingecko.p.rapidapi.com",
+          "x-rapidapi-key":
+            "3324f6e115msh4cc8dab6fc3f512p1ce99bjsn4395c68d95fc",
+          useQueryString: true,
+        },
+        params: {
+          ids: "bitcoin,ethereum",
+          vs_currencies: "usd",
+        },
+      }).catch((error) => console.log(error));
 
-    //   console.log(await price);
-    // };
+      console.log("coin", price.data);
+    };
     fetchData();
-    // fetchPrice();
+    fetchPrice();
   }, []);
 
   return (
