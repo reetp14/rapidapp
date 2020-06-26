de431bc49d9c2cd1f1c7f539155e1861cbab4bf4
Reet Pratayay <reet.p14@gmail.com>
2020-06-26 17:01:15 +0530

    frontend css/ restructured redux reducers

--- src/components/mainList.js
+++ src/components/mainList.js
@@ -3,18 +3,38 @@ import { connect } from "react-redux";
 
 const mainList = (props) => {
   var listItems = props.listitem;
+  console.log(props.item);
 
   return (
     <div>
       <ul className="list-group list-group-flush ">
         {listItems.map((post) => (
-          <li key={post.data.title} className="list-group-item flex-container">
-            <img
-              src={post.data.thumbnail}
-              alt="thumb"
-              className="thumbnail"
-            ></img>
-            <div>{post.data.title}</div>
+          <div className="content">
+            <li
+              key={post.data.title}
+              className="list-group-item flex-container"
+            >
+              <img
+                src={post.data.thumbnail}
+                alt="thumb"
+                className="thumbnail"
+              ></img>
+              <div>{post.data.title}</div>
+            </li>
+            <div className="btn-wrap ">
+              <button type="button" className="btn btn-primary">
+                Show
+              </button>
+            </div>
+          </div>
+        ))}
+
+        {props.item.map((it) => (
+          <li key={it.id} className="list-group-item flex-container">
+            <div>{it.item}</div>
+            <button type="button" className="btn btn-primary">
+              Primary
+            </button>
           </li>
         ))}
       </ul>
